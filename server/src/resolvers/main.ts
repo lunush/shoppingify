2a03eb5d982c3837d6f375a2d7d5a6663a3436a0
Lunush <76045303+lunush@users.noreply.github.com>
2021-05-19 16:26:14 +0300

    Add basic resolver

--- server/src/resolvers/main.ts
+++ server/src/resolvers/main.ts
@@ -1,9 +1,33 @@
-import { Query, Resolver } from "type-graphql";
+import { Field, ObjectType, Query, Resolver } from "type-graphql";
+import { categories, items } from "../data";
+
+@ObjectType()
+class Category {
+  @Field()
+  id: number;
+  @Field()
+  name: string;
+}
+
+@ObjectType()
+class Item {
+  @Field()
+  id: number;
+  @Field()
+  name: string;
+  @Field()
+  category: string;
+}
 
 @Resolver()
 export class MainResolver {
-  @Query(() => String)
-  hello() {
-    return "Hello there";
+  @Query(() => [Item])
+  getItems() {
+    return items;
+  }
+
+  @Query(() => [Category])
+  getCategories() {
+    return categories;
   }
 }
