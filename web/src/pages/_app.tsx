3694cba3b6f9c978bc7128f504bc7d090807a3bd
Lunush <76045303+lunush@users.noreply.github.com>
2021-05-19 20:47:12 +0300

    Get data from backend

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,14 +3,14 @@ import 'tailwindcss/tailwind.css';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://locahost:4040/graphql',
+  uri: 'http://localhost:4040/graphql',
   cache: new InMemoryCache(),
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { useGetItemsQuery } from '@/graphql/generated';
 import Head from 'next/head';
 
 const Home = () => {
+  const { data, loading, error } = useGetItemsQuery();
+
+  if (loading) return <div>Loading..</div>;
+  if (error) return <div>{JSON.stringify(error)}</div>;
+
+  console.log(data);
   return (
     <div>
       <Head>
@@ -8,6 +15,11 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>Shoppingify</div>
+      <div>
+        {data?.getItems.map((item) => (
+          <div>{item.name}</div>
+        ))}
+      </div>
     </div>
   );
 };
