311537ff2af0b869c767ef8606c93e3dc5ec4c60
Lunush <76045303+lunush@users.noreply.github.com>
2021-05-19 19:30:43 +0300

    Add basic frontend

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Shoppingify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Shoppingify</div>
+    </div>
+  );
+};
+
+export default Home;
