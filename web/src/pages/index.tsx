6dc1e5a418235c29d34095c5282390ada9b4cb4c
Lunush <76045303+lunush@users.noreply.github.com>
2021-05-20 18:32:40 +0300

    Add missing pages

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,18 +1,5 @@
-import { useGetItemsQuery } from '@/graphql/generated';
-
 const Home = () => {
-  const { data, loading, error } = useGetItemsQuery();
-
-  if (loading) return <div>Loading..</div>;
-  if (error) return <div>{JSON.stringify(error)}</div>;
-
-  return (
-    <div>
-      {data?.getItems.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
---
+++ web/src/pages/items.tsx
@@ -0,0 +1,18 @@
+import { useGetItemsQuery } from '@/graphql/generated';
+
+const Items = () => {
+  const { data, loading, error } = useGetItemsQuery();
+
+  if (loading) return <div>Loading..</div>;
+  if (error) return <div>{JSON.stringify(error)}</div>;
+
+  return (
+    <div>
+      {data?.getItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Items;
