50183e532efe8b75a0729f34df93f06677e0ac8a
Lunush <76045303+lunush@users.noreply.github.com>
2021-05-30 17:28:52 +0300

    Make drawer a bit bigger

--- web/src/components/ShoppingListDrawer.tsx
+++ web/src/components/ShoppingListDrawer.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 const ShoppingListDrawer: React.FC<Props> = () => {
   return (
-    <div className="flex-none w-80 bg-pink-200 overflow-hidden p-6 py-10">
+    <div className="flex-none w-84 bg-pink-200 overflow-hidden p-6 py-10">
       <div className="relative bg-pink-700 h-32 p-5 rounded-3xl flex">
         <img
           className="absolute w-20 -top-6 left-4"
